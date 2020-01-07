d72b591109c2884df0d9398ceb71b9d18d9facde
Savelieva Katerina <e.savelieva@owox.com>
2020-01-07 17:30:52 +0200

    Ability to print users list another way, and don't touch UsersBirthDays component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
-import UsersBirthDays from "./containers/UsersBirthDays";
+import usersBirthDays from "./containers/UsersBirthDays";
+import UsersList from "./components/user/UsersList";
 
 
-const getColor = (count) => {
-  if (count <= 2) return 'grey'
-  else if (count > 2 && count <= 6) return 'blue'
-  else if (count > 6 && count <= 10) return 'green'
-  else return 'red'
-}
 
 function App() {
+
+  const UsersBirthDays =  usersBirthDays(UsersList)
+
+  const getColor = (count) => {
+    if (count <= 2) return 'grey'
+    else if (count > 2 && count <= 6) return 'blue'
+    else if (count > 6 && count <= 10) return 'green'
+    else return 'red'
+  }
+
   return <UsersBirthDays
       apiUrl="https://yalantis-react-school.herokuapp.com/api/task0/users"
       className="vertical"
--- src/containers/UsersBirthDays.js
+++ src/containers/UsersBirthDays.js
@@ -2,76 +2,77 @@ import React, {Component} from "react";
 
 import TabBar from "../components/tabbar/TabBar";
 import TabBarItem from "../components/tabbar/TabBarItem";
-import UsersList from "../components/user/UsersList";
 import './UsersBirthDays.css'
 
 import * as PropTypes from "prop-types";
 
+function usersBirthDays(UsersComponent) {
 
-class UsersBirthDays extends Component {
+    return class UsersBirthDays extends Component {
 
-    static propTypes = {
-        apiUrl: PropTypes.string.isRequired,
-        className: PropTypes.string,
-        getColor: PropTypes.func
-    }
+        static propTypes = {
+            apiUrl: PropTypes.string.isRequired,
+            className: PropTypes.string,
+            getColor: PropTypes.func
+        }
 
-    static defaultProps = {
-        className: '',
-        getColor: () => {}
-    }
+        static defaultProps = {
+            className: '',
+            getColor: () => {}
+        }
 
-    state = {
-        users: []
-    }
+        state = {
+            users: []
+        }
 
-    componentDidMount() {
-        fetch(this.props.apiUrl)
-            .then(res => res.json())
-            .then(users => this.setState({ users: users }))
+        componentDidMount() {
+            fetch(this.props.apiUrl)
+                .then(res => res.json())
+                .then(users => this.setState({users: users}))
 
-    }
+        }
 
-    mapByMonth(users) {
-        if (users && users.length) {
+        mapByMonth(users) {
+            if (users && users.length) {
 
-            return users.reduce(
-                (map, user) => {
-                    const { dob } = user
-                    const month = new Date(dob).toLocaleString("ru", {month: 'long'})
-                    const monthNum = new Date(dob).toLocaleString("ru", {month: 'numeric'})
+                return users.reduce(
+                    (map, user) => {
+                        const {dob} = user
+                        const month = new Date(dob).toLocaleString("ru", {month: 'long'})
+                        const monthNum = new Date(dob).toLocaleString("ru", {month: 'numeric'})
 
-                    const users = map[monthNum] ? [...map[monthNum].users, user] : [user]
+                        const users = map[monthNum] ? [...map[monthNum].users, user] : [user]
 
-                    return {...map, [monthNum]: {month: month, users: users}}
-                },
-                {}
+                        return {...map, [monthNum]: {month: month, users: users}}
+                    },
+                    {}
+                )
+            }
+            return {}
+        }
+
+
+        render() {
+            const usersMap = this.mapByMonth(this.state.users)
+            const {className, getColor} = this.props
+
+            return (
+                <TabBar className={className} changeByMouseMove>
+                    {Object.keys(usersMap).map((monthNum) => {
+
+                        const {month} = usersMap[monthNum]
+                        const navClassName = getColor(usersMap[monthNum].users.length)
+
+                        return (
+                            <TabBarItem label={month} key={monthNum} navClassName={navClassName}>
+                                <UsersComponent users={usersMap[monthNum].users}/>
+                            </TabBarItem>
+                        )
+                    })}
+                </TabBar>
             )
         }
-        return {}
-    }
-
-
-    render() {
-        const usersMap = this.mapByMonth(this.state.users)
-        const { className, getColor } = this.props
-
-        return (
-            <TabBar className={className} changeByMouseMove>
-                {Object.keys(usersMap).map((monthNum) => {
-
-                    const { month } = usersMap[monthNum]
-                    const navClassName = getColor(usersMap[monthNum].users.length)
-
-                    return (
-                        <TabBarItem label={month} key={monthNum} navClassName={navClassName}>
-                            <UsersList users={usersMap[monthNum].users} />
-                        </TabBarItem>
-                    )
-                })}
-            </TabBar>
-        )
     }
 }
 
-export default UsersBirthDays;
+export default usersBirthDays;
