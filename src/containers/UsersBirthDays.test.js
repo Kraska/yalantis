68c3e082394ec74e354418a726e9d4ec5f123bad
Savelieva Katerina <e.savelieva@owox.com>
2020-01-08 10:38:20 +0200

    Test. Second test case

--- src/containers/UsersBirthDays.test.js
+++ src/containers/UsersBirthDays.test.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
 import { act } from "react-dom/test-utils";
 import usersBirthDays from "./UsersBirthDays";
-import UsersList from "../components/user/UsersList";
 
 
 
@@ -24,12 +23,15 @@ describe('UsersBirthDays', () => {
 
     it("API return 1 user => we have 1 tab", async () => {
 
-        await rendComponent([{
+        const fakeUsers = [{
             id: 'id1',
             firstName: "Joni",
             lastName: " Baez",
             dob: "2019-02-26T16:52:36.244Z"
-        }]);
+        }]
+        await rendComponent(fakeUsers);
+
+        //console.log(container.querySelector(".tab-bar-item").innerHTML)
 
         const navItems = container.querySelectorAll(".nav-item")
         expect(navItems.length).toBe(1)
@@ -37,10 +39,41 @@ describe('UsersBirthDays', () => {
 
         const tabItems = container.querySelectorAll(".tab-bar-item")
         expect(tabItems.length).toBe(1)
-        expect(tabItems[0].innerHTML).toBe("<ul><li>Joni  Baez - 26 февраля 2019 г.</li></ul>");
+        expect(tabItems[0].innerHTML).toBe(JSON.stringify(fakeUsers));
 
     });
 
+
+    it("API return 2 users (birth in feb. and sept.) => we have 2 tabs", async () => {
+
+        const febUser = {
+            id: "5e00928d91e7feaa9872ec08",
+            firstName: "Yang",
+            lastName: "Carson",
+            dob: "2019-02-26T16:52:36.244Z"
+        }
+        const septUser = {
+            id: "5e00928df892b0c84c82db9d",
+            firstName: "Dorsey",
+            lastName: "Meadows",
+            dob: "2019-09-19T09:34:32.083Z"
+        }
+
+        await rendComponent([febUser, septUser]);
+
+        //console.log(container.querySelector(".tab-bar-item").innerHTML)
+
+        const navItems = container.querySelectorAll(".nav-item")
+        expect(navItems.length).toBe(2)
+        expect(navItems[0].textContent).toBe("февраль");
+        expect(navItems[1].textContent).toBe("сентябрь");
+
+        const tabItems = container.querySelectorAll(".tab-bar-item")
+        expect(tabItems.length).toBe(2)
+        expect(tabItems[0].innerHTML).toBe(JSON.stringify([febUser]));
+        expect(tabItems[1].innerHTML).toBe(JSON.stringify([septUser]));
+
+    });
 
     const rendComponent = async(fakeUsers = []) => {
 
@@ -50,12 +83,14 @@ describe('UsersBirthDays', () => {
             })
         );
 
-        const UsersBirthDays = usersBirthDays(UsersList)
+        const UsersBirthDays = usersBirthDays(StubUsersList)
         await act(async () => {
             render(<UsersBirthDays apiUrl="http://test" />, container);
         });
+    }
 
-        // global.fetch.mockRestore();
+    const StubUsersList = ({ users }) => {
+        return JSON.stringify(users)
     }
 });
 
